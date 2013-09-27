430ce277a785c2b8bfd7e44bf9c1f300a1b6b13e
Nick Mathews <nick@codemettle.com>
2013-09-27 08:32:58 -0400

    utility method for temperature conversion

--- weather/src/main/java/com/boboddy/weather/util/Util.java
+++ weather/src/main/java/com/boboddy/weather/util/Util.java
@@ -2,6 +2,12 @@ package com.boboddy.weather.util;
 
 /**
  * Created by boboddy on 9/27/13.
+ *
+ * Contains utility methods
  */
 public class Util {
+
+    public static double celsiusToFahr(double c) {
+        return (c * 1.8) + 32;
+    }
 }
